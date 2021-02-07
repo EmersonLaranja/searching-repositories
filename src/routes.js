19a927387ca8d1db897ca6546b94a79df943f865
EmersonLaranja <emersonlaranja@gmail.com>
2021-02-07 10:36:28 -0300

    default routes config

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        {' '}
+        {/* allow only one page per route */}
+        <Route path="/" component={Main} exact />
+        <Route path="/repository" component={Repository} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
